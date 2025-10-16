7d0ede68dd2e55b39257ecd160706ce43d5ba4ab
DevanZulfangga <devanzulfanga07@gmail.com>
2025-10-16 14:31:45 +0700

    sudah ga eror

--- resources/views/landing-page/landing-page.blade.php
+++ resources/views/landing-page/landing-page.blade.php
@@ -32,7 +32,7 @@
     </div>
  
 <!-- Menu Desktop -->
-<ul class="hidden md:flex space-x-6 font-medium items-center justify-end py-6 text-xl">
+<ul class="hidden md:flex space-x-6 font-medium items-center justify-end py-6 text-xl w-full">
   <li><a href="{{ route('home') }}" class="hover:text-[#a67b5b] hover:underline hover:font-bold transition-colors duration-300">Home</a></li>
   <li><a href="{{ route('menu') }}" class="hover:text-[#a67b5b] hover:underline hover:font-bold transition-colors duration-300">Menu</a></li>
   <li><a href="{{ route('legalitas') }}" class="hover:text-[#a67b5b] hover:underline hover:font-bold transition-colors duration-300">Legalitas</a></li>
@@ -41,19 +41,20 @@
 
   <!-- Tambahkan Keranjang -->
   <li>
-    <a href="{{ route('keranjang') }}" class="flex items-center gap-2 hover:text-[#a67b5b] hover:underline hover:font-bold transition-colors duration-300">
+    <a href="{{ route('home') }}" class="flex items-center gap-2 hover:text-[#a67b5b] hover:underline hover:font-bold transition-colors duration-300">
       <img src="{{ asset('uploads/keranjang.png') }}" alt="Keranjang" class="w-6 h-6">
       Keranjang
     </a>
   </li>
 
-  <li>
+  <!-- Tombol Log In paling kanan -->
+  <li class="ml-auto">
     <a href="{{ route('login') }}" class="bg-[#6f4e37] hover:underline hover:font-bold text-white px-4 py-2 rounded-lg hover:bg-[#5a3e2b] transition-colors duration-300">
       Log In
     </a>
   </li>
 </ul>
-</div>
+
 
 <!-- Mobile Menu -->
 <ul id="mobile-menu" class="md:hidden absolute top-full left-0 w-full flex-col space-y-2 px-4 pb-3 bg-white/90 backdrop-blur-md shadow-md transform -translate-y-2 opacity-0 pointer-events-none transition-all duration-300">
@@ -65,7 +66,7 @@
 
   <!-- Tambahkan Keranjang di Mobile -->
   <li>
-    <a href="{{ route('keranjang') }}" class="flex items-center gap-2 py-2 text-[#6f4e37] hover:text-[#a67b5b] transition-colors duration-300 ">
+    <a href="{{ route('home') }}" class="flex items-center gap-2 py-2 text-[#6f4e37] hover:text-[#a67b5b] transition-colors duration-300 ">
       <img src="{{ asset('uploads/keranjang.png') }}" alt="Keranjang" class="w-6 h-6">
       Keranjang
     </a>
